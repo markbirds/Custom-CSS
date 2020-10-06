a48524b02523ba1e7753acaa3f5d2b3b29035275
Owen <fowenpatrick@gmail.com>
2020-10-06 15:40:58 +0800

    add

--- Navbar/navbar.js
+++ Navbar/navbar.js
@@ -1,13 +1,13 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
-  function myFunction2(x) {
-    x.classList.toggle("change");
-    myFunction();
-  }
+function myFunction2(x) {
+  x.classList.toggle("change");
+  myFunction();
+}
